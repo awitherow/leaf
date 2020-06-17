22a01e56735b974ad6915a6aaab48bc647a52249
awitherow <au.witherow@gmail.com>
2020-06-17 19:25:27 -0400

    implement working view for home page version one

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,27 @@
 import React, { useState } from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  ScrollView,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  AsyncStorage,
+} from "react-native";
 
 import DateSelector from "./components/DateSelector";
+import EveningPrompts from "./components/EveningPrompts";
+import DismissKeyboard from "./components/DismissKeyboard";
 
 import { getFormattedDateString, calculateDay } from "./helpers/dates";
 
 export default function Main() {
+  AsyncStorage.getAllKeys((err, keys) => {
+    AsyncStorage.multiGet(keys, (error, stores) => {
+      stores.map((result, i, store) =>
+        console.log({ [store[i][0]]: store[i][1] })
+      );
+    });
+  });
+
   const [daysFromCurrentDate, setDaysFromCurrentDate] = useState(0);
 
   const today = new Date();
@@ -13,9 +29,24 @@ export default function Main() {
   const currentDay = getFormattedDateString(daySelected);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <DateSelector currentDay={currentDay} setDay={setDaysFromCurrentDate} daysFrom={daysFromCurrentDate} />
-    </SafeAreaView>
+    <DismissKeyboard>
+      <SafeAreaView style={styles.container}>
+        <DateSelector
+          currentDay={currentDay}
+          setDay={setDaysFromCurrentDate}
+          daysFrom={daysFromCurrentDate}
+        />
+        <KeyboardAvoidingView
+          style={{ flex: 1, flexDirection: "column", justifyContent: "center" }}
+          behavior="padding"
+          enabled
+        >
+          <ScrollView>
+            <EveningPrompts day={daySelected} />
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </DismissKeyboard>
   );
 }
 
@@ -23,7 +54,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
