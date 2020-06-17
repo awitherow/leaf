4882eb68f56df7c87cf2f28d76db4762c11b6c27
awitherow <au.witherow@gmail.com>
2020-06-17 19:20:09 -0400

    add evening prompt strings

---
+++ src/helpers/strings.ts
@@ -0,0 +1,20 @@
+export const EVENING_PROMPTS = {
+  wins: {
+    question: "What wins can you celebrate?",
+    description:
+      "These can be anything as simple as having gotten out of bed, successful strides towards your goals, or things you are proud of yourself for accomplishing!",
+  },
+  lessons: {
+    question: "What valuable lessons did you learn?",
+    description:
+      "Try to focus on life lessons from things that happened to you. Perhaps there are some key insights into your day to day interactions or experiences?",
+  },
+  dreams: {
+    question:
+      "If you could have any dreams of yours come true, which would you pick?",
+    description:
+      "Think of those BIG things you want out of life, get descriptive, dream big and shoot for the moon! Even if you miss, you'll land amongst the stars!",
+  },
+};
+
+export const EVENING_PROMPTS_KEYS = Object.keys(EVENING_PROMPTS);
